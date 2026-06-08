c99445fcc30d25ef83eaabc2f9eda939fff67fab
nestor <nestorfacundoguarachi@gmail.com>
2026-06-08 17:41:14 -0300

    empece con la cola de lucas

--- src/TDA/Cola.java
+++ src/TDA/Cola.java
@@ -1,4 +1,25 @@
 package TDA;
 
-public class Cola {
+public class Cola <T> {
+    private static class Nodo <T> {
+        T dato;
+        Nodo <T> siguiente;
+
+        public Nodo(T dato) {
+            this.dato = dato;
+            this.siguiente = null;
+        }
+    }
+
+    private Nodo <T> frente;
+    private Nodo <T> fin;
+    private int tamanio;
+
+
+    public Cola() {
+        fin = null;
+        tamanio = null;
+        tamanio =0;
+    }
+
 }

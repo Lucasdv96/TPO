c3c371c552e59a265bd5ac78ab952594a658b487
Nestor Guarachi <88410435+NestorGuarachi@users.noreply.github.com>
2026-06-28 17:06:04 -0300

    nueva clase Servidor

---
+++ src/Clases/Servidor.java
@@ -0,0 +1,34 @@
+package Clases;
+
+public class Servidor {
+
+    private String id;
+    private String ciudad;
+
+    public Servidor(String id, String ciudad) {
+        this.id = id;
+        this.ciudad = ciudad;
+    }
+
+    public String getId()     { return id; }
+    public String getCiudad() { return ciudad; }
+
+    @Override
+    public String toString() {
+        return "Servidor[" + id + " - " + ciudad + "]";
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Servidor)) return false;
+        Servidor s = (Servidor) o;
+        return this.id.equals(s.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
